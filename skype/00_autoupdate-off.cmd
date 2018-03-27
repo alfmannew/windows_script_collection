c5b42997a874d1fbf8e2bc24be9f7762c63c6653
alfmannew <>
2018-03-27 17:25:29 +0300

    update

--- skype/00_autoupdate-off.cmd
+++ skype/00_autoupdate-off.cmd
@@ -1,11 +1,13 @@
-rem echo off
+@echo off
 
 :: autoupdate-off
-:: ::http://community.skype.com/t5/Windows-desktop-client/How-to-disable-Skype-updates/td-p/6710/highlight/true/page/4
+:: http://community.skype.com/t5/Windows-desktop-client/How-to-disable-Skype-updates/td-p/6710/highlight/true/page/4
 ::
 
-del /F /Q /A %TEMP%\SkypeSetup.exe
-echo > "%TEMP%\SkypeSetup.exe"
-attrib +R +A +S +H +I "%TEMP%\SkypeSetup.exe"
+set my_name="SkypeSetup.exe"
 
-pause
+del /F /Q /A "%TEMP%\%my_name%"
+echo ^1> "%TEMP%\%my_name%"
+attrib +R +A +S +H +I "%TEMP%\%my_name%"
+
+::pause
